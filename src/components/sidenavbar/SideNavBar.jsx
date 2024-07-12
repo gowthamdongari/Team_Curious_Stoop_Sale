de76abc5bfa92949ac45401664f5674067b309cc
Gowtham Dongari <gowthamdongari@gmail.com>
2024-07-11 22:56:51 -0500

    side nav bar

--- src/components/sidenavbar/SideNavBar.jsx
+++ src/components/sidenavbar/SideNavBar.jsx
@@ -18,9 +18,9 @@ function SideNavBar() {
       >
         <HiX />
       </span> */}
-      <div className={`mx-[56px] mt-[50px] flex items-center`}>
-        <div className="mt-1 ml-1 h-2.5 mb-5 font-poppins text-[26px] font-bold uppercase text-navy-700 dark:text-white">
-          Brooklyn <span className="font-medium mb-4"> <br/> Stoop Sale</span>
+      <div className={`mx-[40px] mt-[50px] flex items-center`}>
+        <div className="mt-1 ml-1 h-2.5 mb-5 font-poppins text-[22px] font-bold uppercase text-navy-700 dark:text-white">
+          Brooklyn <span className="font-medium mb-2"> <br/> Stoop Sale</span>
         </div>
       </div>
       <div className="mt-[58px] mb-7 h-px bg-gray-300 dark:bg-white/30"/>
